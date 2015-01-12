9c75ca06760fe7151970796dab212ced7b958a2f
stone725 <stone_725@outlook.jp>
2015-01-12 09:10:02 +0900

    エラトステネスの篩を若干調整

--- Library/Sieve_of_Eratosthenes.cpp
+++ Library/Sieve_of_Eratosthenes.cpp
@@ -1,13 +1,13 @@
-//maxまでの素数を渡した配列に追加する関数
+//nまでの素数を渡した配列に追加する関数
 #include <vector>
 using namespace std;
 
-void sieve_of_eratosthenes( vector<int> &pm, int max ) {
-	vector<bool> is_pm( max + 1, true );
+void sieve_of_eratosthenes( vector<int> &pm, int n ) {
+	vector<bool> is_pm( n + 1, true );
 	is_pm[ 0 ] = is_pm[ 1 ] = false;
-	for ( int i = 2; i * i <= max; i += 2 ) {
+	for ( int i = 2; i * i <= n; i += 2 ) {
 		if ( is_pm[ i ] ) {
-			for ( int j = i * i; j <= max; j += i ) {
+			for ( int j = i * i; j <= n; j += i ) {
 				is_pm[ j ] = false;
 				if ( i != 2 ) {
 					j += i;
@@ -18,7 +18,7 @@ void sieve_of_eratosthenes( vector<int> &pm, int max ) {
 			}
 		}
 	}
-	for ( int i = 2; i <= max; i += 2 ) {
+	for ( int i = 2; i <= n; i += 2 ) {
 		if ( is_pm[ i ] ) {
 			pm.push_back( i );
 			if ( i == 2 ) {
