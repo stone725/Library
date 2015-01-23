c7927e5df732c639ddac6d9034718b87be6596ae
stone725 <stone_725@outlook.jp>
2015-01-24 02:40:39 +0900

    ワーシャルフロイドを調整
    英語できなくてつらい

--- Library/Warshall_Floyd.cpp
+++ Library/Warshall_Floyd.cpp
@@ -1,10 +1,10 @@
-//それぞれの経路の長さが入力されたrootをもとにその全点を最短経路で返すように変更する関数
+//それぞれの経路の長さが入力されたrouteをもとにその全点を最短経路で返すように変更する関数
 
-void warshall_floyd( vector<vector<int> > &root ) {
-	for ( int i = 0; i < root.size(); i++ ) {
-		for ( int j = 0; j < root.size(); j++ ) {
-			for ( int k = 0; k < root.size(); k++ ) {
-				root[ j ][ k ] = root[ k ][ j ] = min( root[ j ][ k ], root[ j ][ i ] + root[ i ][ k ] );
+void warshall_floyd( vector<vector<int> > &route ) {
+	for ( int i = 0; i < route.size(); i++ ) {
+		for ( int j = 0; j < route.size(); j++ ) {
+			for ( int k = 0; k < route.size(); k++ ) {
+				route[ j ][ k ] = min( route[ j ][ k ], route[ j ][ i ] + route[ i ][ k ] );
 			}
 		}
 	}
